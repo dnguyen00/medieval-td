bb84ccfc294fefcbb5555ff98a83a70961e8c225
dnguyen00 <73767929+dnguyen00@users.noreply.github.com>
2023-11-01 11:30:43 +0000

    added files

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:medieval_td/medieval_td.dart';
+
+void main() {
+  runApp(GameWidget(
+    game: MedievalTD(),
+  ));
+}
---
+++ lib/medieval_td.dart
@@ -0,0 +1,3 @@
+import 'package:flame/game.dart';
+
+class MedievalTD extends FlameGame {}
