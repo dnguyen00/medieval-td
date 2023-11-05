f7ae37f8cb20c5eb2b5a046e6fd90d2465481fbe
dnguyen00 <73767929+dnguyen00@users.noreply.github.com>
2023-11-05 11:53:36 +0000

    updated formatting for tutorial

--- lib/tutorial.dart
+++ lib/tutorial.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 import 'package:medieval_td/medieval_td.dart';
 
@@ -8,11 +11,9 @@ class Tutorial extends Component
   @override
   Future<void> onLoad() async {
     addAll([
-      TextBoxComponent(
-        text: "Do you want to do a tutorial",
-        textRenderer: TextPaint(style: const TextStyle(fontSize: 32)),
-        align: Anchor.topCenter,
-      ),
+      TextComponent()
+        ..text = "Do you want to do a tutorial?"
+        ..textRenderer = TextPaint(style: const TextStyle(fontSize: 32)),
     ]);
   }
 }
