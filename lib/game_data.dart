f1fcd897e44be0368fb33f8df4c6e08e837884dc
dnguyen00 <73767929+dnguyen00@users.noreply.github.com>
2023-12-04 02:18:07 -0500

    added static game data

---
+++ lib/game_data.dart
@@ -0,0 +1,21 @@
+class GameData {
+  static int money = 100;
+  static int arrowDamage = 5;
+  static int houseHealth = 5;
+  static GameOverData data = GameOverData(score: 0, moneyGained: 0);
+}
+
+class GameOverData {
+  int score = 0;
+  int moneyGained = 0;
+
+  GameOverData({required this.score, required this.moneyGained});
+
+  int getScore() {
+    return score;
+  }
+
+  int getMoneyGained() {
+    return moneyGained;
+  }
+}
