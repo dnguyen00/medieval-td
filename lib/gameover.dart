53f59d7332bc5266217a03700b3567d0defed47e
Jahmaria Stevens <137231675+JamProgram@users.noreply.github.com>
2023-12-04 15:53:12 -0500

    Add files via upload
    game over screen that navs to main menu and game

---
+++ lib/gameover.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:medieval_td/levelSelect.dart';
+import 'package:medieval_td/main_menu.dart';
+import 'package:medieval_td/medieval_td.dart';
+
+
+
+class Gameover extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GameOverPage(),
+    );
+  }
+}
+
+class GameOverPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Game Over'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Game Over!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Score: 100', // Replace with the actual score value
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Money Gained: \$50', // Replace with the actual money gained value
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the main menu
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MainMenu()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 60),
+              ),
+              child: Text('Return to Main Menu'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the game screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LevelSelectionScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 60),
+              ),
+              child: Text('Replay Game'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
